a122e4097774bebca84e23f23ae2b26fb3f88d59
Ronald Kinch <ronaldkinch@comcast.net>
2015-08-16 03:03:15 +0000

    Solve exercies chapter 3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,35 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  def setup
+    @base_title = " | Ruby on Rails Tutorial Sample App"
+    @views = [:home, :help, :about, :contact]
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  test "should get view" do
+    @views.each do |view|
+      get view
+      assert_response :success
+      assert_select "title", "#{view.capitalize}#{@base_title}"
+    end
   end
-
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
+  
+#  test "should get home" do
+#    get :home
+#    assert_response :success
+#    assert_select "title", "Home#{@base_title}"
+#  end
+#
+#  test "should get help" do
+#    get :help
+#    assert_response :success
+#    assert_select "title", "Help#{@base_title}"
+#  end
+#
+#  test "should get about" do
+#    get :about
+#    assert_response :success
+#    assert_select "title", "About#{@base_title}"
+#  end
 
 end
