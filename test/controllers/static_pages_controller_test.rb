23896677883651ac8347dc6fdaddbabb28f07646
Ronald Kinch <ronaldkinch@comcast.net>
2015-08-23 00:22:21 +0000

    Finish User model with secure passwords

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,22 +3,20 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
-    @views = [:help, :about, :contact]
+    @views = [:home, :help, :about, :contact]
   end
 
   test "should get view" do
     @views.each do |view|
       get view
       assert_response :success
-      assert_select "title", "#{view.capitalize} | #{@base_title}"
+      title = css_select("title").text
+      if title.include? "|"
+        assert_equal title, "#{view.capitalize} | #{@base_title}"
+      else
+        assert_equal title, "#{@base_title}"
+      end
     end
-  end
-
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-    # assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
 end
